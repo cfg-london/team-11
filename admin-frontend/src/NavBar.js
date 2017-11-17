4b87452b2120aa223e72ef54199dff21a429fef2
mortuie <jimmybob-leon@hotmail.co.uk>
2017-11-17 19:54:57 +0000

    Adding the start of the styling of the login page of the page

---
+++ admin-frontend/src/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, css} from 'aphrodite';
+
+
+export default class NavBar extends React.Component {
+
+
+
+	render() {
+		return <div>banter</div>;
+	}
+}
