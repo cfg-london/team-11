b58488729e13bda24469d7157370019d5e7ebda3
darrylbutcher <darrylbutcher@hotmail.com>
2017-11-17 19:51:45 +0000

    Created all the files

--- app/LinkAge+/App.js
+++ app/LinkAge+/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator, } from 'react-navigation';
+import Home from './Home';
+import Settings from './Settings';
+import About from './About';
+import TableView from './TableView';
+
+
+
+{/*https://reactnavigation.org/docs/intro/*/}
+const Apps = StackNavigator({
+  HomeScreen: { screen: Home },
+  SettingsScreen: { screen: Settings },
+  AboutScreen: { screen: About },
+  TableViewScreen: { screen: TableView },
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Apps/>
       </View>
     );
   }
@@ -16,8 +29,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
