50a8eb2782db37e3a257cf1a75755b0b7dc80327
Atul <kkmonlee@gmail.com>
2017-11-17 23:00:16 +0000

    Added user login and register func

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,3 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('referee','RefereeController');
+
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
