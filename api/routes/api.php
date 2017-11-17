23b1d765b3cc4a31c0987a271f43c60ef6e085c5
SeanJackson1998 <skatts123@gmail.com>
2017-11-17 22:43:59 +0000

    References CRUD

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('referee','RefereeController');
+Route::resource('reference','ReferenceController');
