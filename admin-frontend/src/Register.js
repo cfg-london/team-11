ca90bc815e22c4a13aa067ea53016e6ee88b73bd
mortuie <jimmybob-leon@hotmail.co.uk>
2017-11-18 00:12:31 +0000

    Trying another XHR

--- admin-frontend/src/Register.js
+++ admin-frontend/src/Register.js
@@ -15,21 +15,18 @@ export default class Register extends React.Component {
 
 	registerAccount = () => {
 
-		var data = null;
 		var xhr = new XMLHttpRequest();
-		xhr.withCredentials = true;
-
-		xhr.addEventListener("readystatechange", function () {
-		  if (this.readyState === 4 || this.readyState === 200) {
-		    console.log(this.responseText);
-		  } else if (this.readyState === 401) {
-		  	console.log(this.responseText);
-		  }
-		});
-
-		xhr.open("POST", "http://138.68.150.49/api/register?user_name=" + this.state.username + "&password=" + this.state.password + "&c_password=" + this.state.cpassword);
-		xhr.send(data);
-
+        var data = "user_name=" + this.state.username + "&password=" + this.state.password + "&c_password=" + this.state.cpassword;
+        xhr.addEventListener("readystatechange", function() {
+            if (this.readyState === 4 || this.readyState === 200) {
+                console.log(this.responseText);
+            } else if (this.readyState === 401) {
+            	console.log(this.responseText);
+            }
+        });
+        xhr.open("POST", "http://138.68.150.49/api/register");
+        xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+        xhr.send(data);
 
 
 
@@ -38,19 +35,6 @@ export default class Register extends React.Component {
 
 
 
-		// var http = new XMLHttpRequest();
-		// var url = "http://138.68.150.49/api/register";
-		// var params = "user_name=" + this.state.username + " &password=" + this.state.password + "&c_password=" + this.state.cpassword;
-		// http.open("POST", url, true);
-
-		// http.onreadystatechange = function() {//Call a function when the state changes.
-		// 	if(http.readyState == 4 && http.status == 200) {
-		// 		alert(http.responseText);
-		// 	} else if (http.readyState === 401) {
-		// 		alert(http.responseText);
-		// 	}
-		// }
-		// http.send(params);
 	}
 
 	render() {
