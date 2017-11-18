c3c2ca5b17ad9af2f83937c02d21d1031a23eb63
mortuie <jimmybob-leon@hotmail.co.uk>
2017-11-18 04:05:59 +0000

    Building agaiN

--- admin-frontend/src/View.js
+++ admin-frontend/src/View.js
@@ -3,11 +3,55 @@ import {StyleSheet, css} from 'aphrodite';
 
 export default class View extends React.Component {
 
+	constructor() {
+		super();
+		this.state = {
+			allStatements: {},
+		};
+	}
+
+
+	componentWillMount() {
+
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = (e) => {
+		  if (request.readyState !== 4) {
+		    return;
+		  }
+
+		  if (request.status === 200) {
+		    console.log('success', request.responseText);
+		  } else {
+		    console.warn('error');
+		  }
+		};
+
+		request.open('GET', 'http://138.68.150.49/api/reference');
+		request.setRequestHeader('Content-Type', 'application/json')
+		request.send();
+
+
+	}
 
 
 	render() {
 
 
-		return <div>banter</div>;
+		return (
+			<div>
+
+
+
+			</div>
+
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+
+
+
+
+
+});
