9f49fe0355b7b0d3bd0f274a1fb5c1d61c56592e
darrylbutcher <darrylbutcher@hotmail.com>
2017-11-18 05:47:01 +0000

    fixed my git messup

--- app/LinkAge/AboutMe.js
+++ app/LinkAge/AboutMe.js
@@ -4,24 +4,40 @@ import {
     Text,
     TextInput,
     View,
-    StyleSheet,
+        StyleSheet,
     AsyncStorage,
     Picker,
   } from 'react-native';
 
+import {
+  StackNavigator,
+} from 'react-navigation';
 import { Button } from 'react-native-elements'
-import {StackNavigator,} from 'react-navigation';
 import Countries from './Countries';
+
+
+
 import { PowerTranslator, ProviderTypes, Translation } from 'react-native-power-translator';
 
+const navigationOptions = {header: null }
+
 export default class AboutMe extends React.Component {
+
+  static navigationOptions = ({ navigation, screenProps }) => ({
+      headerTitle: "Tell us about yourself",
+      headerStyle: {
+        backgroundColor: '#FF8E00'
+      },
+      headerTintColor: '#FFFFFF',
+      /*headerRight: <Button title="Settings" onPress={() => navigation.navigate('SettingsScreen')} />,*/
+  });
 
     constructor(props) {
       super(props);
     this.state = {
       language: "en",
       name: "",
-      profession: "",
+      profession: "Police",
     }
     Translation.setConfig(ProviderTypes.Google, 'AIzaSyA0DMZ38W76bNFkkU-l5Op_hPJBnZFQJ74',this.state.language);
 
@@ -62,18 +78,18 @@ export default class AboutMe extends React.Component {
         }
         // Checks if the data has been found
         if (request.status === 200) {
-          (JSON.parse(request.responseText));
+          console.log(JSON.parse(request.responseText));
           try {
-            await AsyncStorage.setItem('refereeID', JSON.parse(request.responseText).referee_id);
+            await AsyncStorage.setItem('refereeID', "" + JSON.parse(request.responseText).referee_id);
           } catch (error) {
-
+            console.warn(error);
           }
 
         } else {
           console.warn('Data not found');
         }
       };
-      request.open('POST', 'http://138.68.150.49/api/referee?name=' + this.state.name + '&profession=' + this.state.profession);
+      request.open('POST', 'http://138.68.150.49/api/referee?name=' + "this.state.name" + '&profession=' + "this.state.profession" + '&phone=' + "0");
       request.send();
 
   }
@@ -91,10 +107,6 @@ export default class AboutMe extends React.Component {
 
       }
     }
-
-    update(){
-      console.log(this.state.profession);
-    }
     
     render() {
     const { navigate } = this.props.navigation;
@@ -104,7 +116,7 @@ export default class AboutMe extends React.Component {
         <PowerTranslator style={{fontSize: 22, fontWeight:'bold'}} text={'Name'} />
         <TextInput placeholder='Name' onChangeText={(name) => this.setState({name})} style={styles.input} />
         <PowerTranslator style={{fontSize: 22, fontWeight:'bold'}} text={'Profession'} />
-        <Picker style={{fontSize: 20}} selectedValue = {this.state.profession} onValueChange = {this.update.bind(this)}>
+        <Picker selectedValue = {this.state.profession} onValueChange={(profession) => this.setState({profession})}>
           <Picker.Item label = "Police" value = "Police" />
           <Picker.Item label = "Fire and Rescue" value = "Fire and Rescue" />
           <Picker.Item label = "Ambulance" value = "Ambulance" />
@@ -116,6 +128,8 @@ export default class AboutMe extends React.Component {
           <Picker.Item label = "Friend, family or neighbour" value = "Friend, family or neighbour" />
           <Picker.Item label = "Other" value = "Other" />
         </Picker>        
+        <PowerTranslator style={{fontSize: 22, fontWeight:'bold'}} text={'Phone'} />
+        <TextInput placeholder='Phone'  keyboardType={'phone-pad'} onChangeText={(phone) => this.setState({phone})} style={styles.input} />
         <View style={{margin:7}} />
         <Button 
           onPress={()=> this.next()}
@@ -140,11 +154,5 @@ const styles = StyleSheet.create({
   input:{
     fontSize: 20,
     height: 50,
-  },
-  title:{
-    fontSize:40,
-  },
-  header:{
-    margin:20,
   }
 });
